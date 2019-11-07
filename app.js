d5dc07137a56de6f3e052b73acfc6de977a0c0cc
xilatleo <trinhhoangquan234@gmail.com>
2019-11-07 15:55:45 -0600

    'Final'

--- app.js
+++ app.js
@@ -16,13 +16,56 @@ app.post('/', function(req,res){
     var lastName = req.body.lName;
     var email = req.body.email;
 
-    console.log(fistName);
-    console.log(lastName);
-    console.log(email);
-    
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fistName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+     var jsonData = JSON.stringify(data);
+
+    var options = {
+        url:'https://us5.api.mailchimp.com/3.0/lists/3cb5d4d8f5',
+        method: 'POST',
+        headers: {
+            'Authorization' : 'quan1 ed673bb5509e7a34f63853d574f9b2aa-us5',        
+        },
+         body: jsonData
+    }
+    request(options, function(error, response,body){
+        if(error){
+           res.sendFile(__dirname + '/failure.html');
+            
+        }else{
+            if(response.statusCode === 200){
+                res.sendFile(__dirname + '/success.html');
+            }else {
+                res.sendFile(__dirname + '/failure.html');
+            }
+            
+        }
+    })
+
     
 })
-app.listen(3000, function(){
+
+app.post('/failure',function(req,res){
+    res.redirect('/');
+});
+
+app.post('/success',function(req,res){
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log('app is listening to port 3000');
     
 });
+//ed673bb5509e7a34f63853d574f9b2aa-us5
+//3cb5d4d8f5
